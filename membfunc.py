ebe149662970828ff467afaed40f8f297eaeb53b
kurotsoccer721 <39691535+kurotsoccer721@users.noreply.github.com>
2018-09-05 16:07:55 +0900

    membfucn
    add 4points

--- membfunc.py
+++ membfunc.py
@@ -12,20 +12,35 @@ def low3(i,s3):
         return 0.0
 
 def high3(i,s3):
-    if (i < s3[0,0]):
+    if (i <= s3[0,0]):
         return 0.0
     elif (i <= s3[1,0]):
         return s3[1,1]*(i-s3[0,0])/(s3[1,0]-s3[0,0])
-    elif(i <= s3[2,0]):
-        return (1-s3[1,1])*(i-s3[0,3])/(s3[0,3]-s3[0,2])+1
+    elif(i < s3[2,0]):
+        return (1-s3[1,1])*(i-s3[2,0])/(s3[2,0]-s3[1,0])+1
     else:
         return 1.0
 
+def low4(i,s4):
+    if (i <= s4[0,0]):
+        return 1.0
+    elif (i <= s4[1,0]):
+        return (s4[1,1]-1)*(i-s4[0,0])/(s4[1,0]-s4[0,0])+1
+    elif(i <= s4[2,0]):
+        return (s4[2,1]-s4[1,1])*(i-s4[1,0])/(s4[2,0]-s4[1,0])+s4[1,1]
+    elif(i < s4[3,0]):
+        return (-s4[2,1])*(i-s4[3,0])/(s4[3,0]-s4[2,0])
+    else:
+        return 0.0
 
-a = np.arange(6,dtype=float).reshape(3,2)
-a[0,0] = 0.25
-a[1,0] = 0.5
-a[2,0] = 0.75
-a[1,1] = 0.5
-
-print(high3(0.49,a))
+def high4(i,s4):
+    if (i <= s4[0,0]):
+        return 0.0
+    elif (i <= s4[1,0]):
+        return s4[1,1]*(i-s4[0,0])/(s4[1,0]-s4[0,0])
+    elif(i <= s4[2,0]):
+        return (s4[2,1]-s4[1,1])*(i-s4[1,0])/(s4[2,0]-s4[1,0])+s4[1,1]
+    elif(i < s4[3,0]):
+        return (1-s4[2,1])*(i-s4[3,0])/(s4[3,0]-s4[2,0])+1
+    else:
+        return 1.0
